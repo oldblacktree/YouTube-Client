730bfd6318ec3066c59645e1fd6ac4052b4354e4
Danil Lukonin <OldBlackTree@gmail.com>
2019-05-19 21:30:24 +0300

    refactor: refactoring

--- youtube-client/src/controllers/App.js
+++ youtube-client/src/controllers/App.js
@@ -12,10 +12,6 @@ function start() {
 
   view.searchBox.element.addEventListener('keyup', async () => {
     if (event.keyCode === 13) {
-      // let g = await document.getElementsByClassName('greeting')[0];
-      // g.classList.add('display-none');
-      // document.getElementById('clips-box').innerHTML = '';
-      // document.getElementById('clips-box').style.setProperty('--i', '0');
       view.resetPosition();
       request = view.searchBox.element.value;
       model = await new AppModel(request);
@@ -29,13 +25,3 @@ function start() {
 }
 
 export default start;
-
-
-// async start() {
-//   const model = new AppModel(this.state);
-//   const data = await model.getClipNames();
-//   const view = new AppView(data);
-
-//   view.render();
-// }
-// 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyDRiMRQqTS_lqEQ8vsicotuSzhTlnaEhc0&type=video&part=snippet&maxResults=15&q=js
--- youtube-client/src/models/AppModel.js
+++ youtube-client/src/models/AppModel.js
@@ -1,6 +1,6 @@
 export default class AppModel {
   constructor(request) {
-    this.url = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyD30CHOLJv82UNtr4cb5V3smX6Kf0RqE8Y&type=video&part=snippet&maxResults=15&q=${request}`;
+    this.url = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyA8shEa0MdEU06gOwbZYyYZRpoFIa3Y2QU&type=video&part=snippet&maxResults=15&q=${request}`;
   }
 
   async getClips() {
@@ -10,9 +10,9 @@ export default class AppModel {
     const arrVideoId = dataId.items.map(clip => clip.id.videoId);
 
     const responceAllData = await fetch(
-      `https://www.googleapis.com/youtube/v3/videos?key=AIzaSyD30CHOLJv82UNtr4cb5V3smX6Kf0RqE8Y&id=${arrVideoId.join(
-        ','
-      )}&part=snippet,statistics`
+      `https://www.googleapis.com/youtube/v3/videos?key=AIzaSyA8shEa0MdEU06gOwbZYyYZRpoFIa3Y2QU&id=${arrVideoId.join(
+        ',',
+      )}&part=snippet,statistics`,
     );
     const allData = await responceAllData.json();
 
@@ -24,71 +24,10 @@ export default class AppModel {
         description: clip.snippet.description,
         author: clip.snippet.channelTitle,
         view: clip.statistics.viewCount,
-        id: clip.id
+        id: clip.id,
       };
       return clipData;
     });
     return arrClipData;
   }
 }
-
-
-// static extractClips(data) {
-//   console.log('data from response', data);
-//   const videoId = [];
-
-//   let arrClipData = data.items.map((clip) => {
-//     videoId.push(clip.id.videoId);
-//     const clipData = {
-//       image: clip.snippet.thumbnails.medium.url,
-//       date: clip.snippet.publishedAt.slice(0,10),
-//       title: clip.snippet.title,
-//       description: clip.snippet.description,
-//       author: clip.snippet.channelTitle,
-//     };
-
-//     console.log('полученный массив готовых данных', arrClipData)
-//     return clipData;
-//   });
-// }
-// async getClipsData() {
-//   const responce = await fetch(this.url);
-//   const data = await responce.json();
-//   return AppModel.extractClips(data);
-// }
-// const videoId = clip.id.videoId;
-// const viewdata = fetch(
-//   `https://www.googleapis.com/youtube/v3/videos?key=AIzaSyDRiMRQqTS_lqEQ8vsicotuSzhTlnaEhc0&id=${videoId}&part=snippet,statistics`
-// );
-// console.log('viewdata ', viewdata);
-// const views = viewdata.json()
-
-
-// async getClips() {
-//   const responce = await fetch(this.url);
-//   const data = await responce.json();
-//   const arrVideoId = [];
-
-//   const arrClipData = data.items.map((clip) => {
-//     arrVideoId.push(clip.id.videoId);
-//     const clipData = {
-//       image: clip.snippet.thumbnails.medium.url,
-//       date: clip.snippet.publishedAt.slice(0, 10),
-//       title: clip.snippet.title,
-//       description: clip.snippet.description,
-//       author: clip.snippet.channelTitle,
-//     };
-//     return clipData;
-//   });
-
-//   const responseForViews = await fetch(
-//     `https://www.googleapis.com/youtube/v3/videos?key=AIzaSyDRiMRQqTS_lqEQ8vsicotuSzhTlnaEhc0&id=${arrVideoId.join(',')}&part=snippet,statistics`
-//   );
-//   const dataViews = await responseForViews.json();
-//   const arrViews = dataViews.items.map((clip) => {
-//     return clip.statistics.viewCount
-//   })
-//   console.log('arrClipData', arrClipData);
-//   console.log('arrVieoId', arrVideoId);
-//   console.log('arrViews', arrViews);
-// }
